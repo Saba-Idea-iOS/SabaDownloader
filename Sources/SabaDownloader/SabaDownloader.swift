32f03393464a440ef99399207ce56a00ba121282
aakpro <aakpro@gmail.com>
2022-02-27 12:24:28 +0100

    first commit

--- Sources/SabaDownloader/SabaDownloader.swift
+++
@@ -1,6 +0,0 @@
-public struct SabaDownloader {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- Tests/SabaDownloaderTests/SabaDownloaderTests.swift
+++ Tests/SabaDownloaderTests/SabaDownloaderTests.swift
@@ -2,10 +2,5 @@ import XCTest
 @testable import SabaDownloader
 
 final class SabaDownloaderTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SabaDownloader().text, "Hello, World!")
-    }
+
 }
